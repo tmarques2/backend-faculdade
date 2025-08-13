45e582c208032d687bdf57e3a0b8393ff1803322
tmarques2 <thainaramarques258@gmail.com>
2025-08-13 20:42:03 -0300

    aula02
    exibindo as variaveis no terminal

--- Aula02/ex1.js
+++ Aula02/ex1.js
@@ -3,4 +3,8 @@
 const idade = 18;
 const nome = "Thainara";
 
+//Imprime msg no terminal
 console.log("Ola Thainara");
+
+//Exibindo valor das constantes
+console.log(`${nome} idade ${idade}`);      //exibe valor no terminal (shift + tecla do lado do P)
