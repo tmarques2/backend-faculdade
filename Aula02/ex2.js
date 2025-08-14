965fb45f173cb2498f79309f438147aeb444510e
tmarques2 <thainaramarques258@gmail.com>
2025-08-13 21:42:05 -0300

    Create ex2.js
    tipos de variavel

---
+++ Aula02/ex2.js
@@ -0,0 +1,8 @@
+// Verificando o tipo de variavel
+
+const valor = 50.5;
+const num = 100;
+const a = true;
+console.log(valor, `Tipo de constante ${typeof valor}`);
+console.log(num, `Tipo de constante ${typeof num}`)
+console.log(a, `Tipo de constante ${typeof a}`)
