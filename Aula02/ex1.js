30c93fba6d4c9a47beaa63442db41d1f74021969
tmarques2 <thainaramarques258@gmail.com>
2025-08-13 21:24:54 -0300

    Update ex1.js

--- Aula02/ex1.js
+++ Aula02/ex1.js
@@ -2,9 +2,11 @@
 
 const idade = 18;
 const nome = "Thainara";
+const inf = nome + " " + idade
 
 //Imprime msg no terminal
 console.log("Ola Thainara");
 
 //Exibindo valor das constantes
 console.log(`${nome} idade ${idade}`);      //exibe valor no terminal (shift + tecla do lado do P)
+console.log(inf);
