0b4ded53239e5648518d4bfce84065e00022b7b4
tmarques2 <thainaramarques258@gmail.com>
2025-08-20 22:41:50 -0300

    exercicio api

--- Aula03/exercicio/server.js
+++ Aula03/exercicio/server.js
@@ -1,11 +1,15 @@
+//Importar express
 const express = require('express');
+//Criar instancia
 const app = express();
+//Definir porta
 const port = 3000;
 
+//Criando rota 
 app.get('/', (req,res)=>{
     res.send("Bem-vindo à minha primeira API! - Thainara");
 });
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`);
-})
+});
