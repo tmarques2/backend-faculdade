fa9cb0f16c419613e008277c2edd498157e986d2
tmarques2 <thainaramarques258@gmail.com>
2025-08-20 22:36:38 -0300

    exercicio api

--- Aula03/exercicio/server.js
+++ Aula03/exercicio/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res)=>{
-    res.send("Bem-vindo à minha primeira API!");
+    res.send("Bem-vindo à minha primeira API! - Thainara");
 });
 
 app.listen(port, ()=>{
