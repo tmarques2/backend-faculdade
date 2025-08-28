10f87a95a32c1913458732d87ea9a5cfbf3f3847
tmarques2 <thainaramarques258@gmail.com>
2025-08-27 22:59:42 -0300

    aula 4

--- Aula04/api_weather/server.js
+++ Aula04/api_weather/server.js
@@ -37,4 +37,87 @@ app.get('/weather', async(req,res)=>{
     }
 });
 
+// criando uma rota multiple  para exibir mais de uma cidade
+
+/*
+app.get('/multiple',async(req,res) => {
+    const {cities}= req.query;
+    // verifico se cities esta vazio
+    if(!cities){
+     return res.status(400).json({error:'Informe pelo menos uma cidade'})
+    }
+    const cityList = cities.split(',');
+    const results =[]; // Lista para armazenar os resultados
+    try {
+        for (let city of cityList){
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}&units=metric&lang=pt_br`);
+        
+        // results.push o push serve para adicionar elementos na lista
+        results.push({city,temperature: response.data.main?.temp ??0,
+            weather: response.data.weather?.[0]?.description??'Desconhecido'});
+        
+    }
+    res.json(results);
+    
+}  catch(err){
+        res.status(500).json({error:'Erro ao consultar api para multiplas cidades'});
+     
+    }
+
+});
+*/
+
+app.get('/multiple', async (req, res) => {
+    const { cities } = req.query;
+
+    if (!cities) {
+        return res.status(400).json({ error: 'Informe pelo menos cidade.'});
+    }
+
+    const cityList = cities.split(',');
+    const results = [];
+
+    try {
+        for (let city of cityList) {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`
+            );
+
+        results.push({
+            city,
+            temperature: response.data.main?.temp ?? 0,
+            weather: response.data.weather?.[0]?.description ?? 'Desconhecido'
+        });
+        }
+
+        res.json(results);
+        
+    }   catch (err) {
+        res.status(500).json({ error: 'Erro ao consultar API para múltiplas cidades.'})
+    }    
+});
+
+
+app.get('/alert', async (req, res) => {
+    const { city, country } = req.query;
+
+    if (!city || !country) {
+        return res.status(400).json({ error: 'Informe cidade e país.'});
+    }
+
+    try {
+        const response = await axios.get (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric&lang=pt_br`
+        );
+
+        const temp = response.data.main?.temp ?? 0;
+
+        let alert = temp > 30 ? 'Quente' : temp < 10 ? 'Frio' : 'Agradavel';
+
+        res.json({ city, temperature: temp, alert});
+
+    }   catch (err) {
+        res.status(500).json({ error: 'Erro ao obter dados do clima.'})
+    } 
+});
+
 app.listen(PORT,()=>console.log(`Servidor rodando em http://localhost:${PORT}`));
