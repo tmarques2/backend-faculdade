88aaccb00e79808648422ca34b18380cb480b4f6
tmarques2 <thainaramarques258@gmail.com>
2025-09-19 16:56:00 -0300

    aula7

--- Aula07/exercicios.js
+++ Aula07/exercicios.js
@@ -49,88 +49,148 @@
 // pessoa1.exibetrabalho("Bosch", 1);
 
 
-//Exercicio 3
-// Classe mãe
-class Automovel {
-    constructor(cor, modelo, combustivel, rodas) {
-        this.cor = cor;
-        this.modelo = modelo;
-        this.combustivel = combustivel;
-        this.rodas = rodas;
-    }
+// //Exercicio 3
+// // Classe mãe
+// class Automovel {
+//     constructor(cor, modelo, combustivel, rodas) {
+//         this.cor = cor;
+//         this.modelo = modelo;
+//         this.combustivel = combustivel;
+//         this.rodas = rodas;
+//     }
 
-    info() {
-        return `${this.modelo} (${this.cor}) - Combustível: ${this.combustivel}, Rodas: ${this.rodas}`;
-    }
-}
+//     info() {
+//         return `${this.modelo} (${this.cor}) - Combustível: ${this.combustivel}, Rodas: ${this.rodas}`;
+//     }
+// }
 
-// Classe filha: Carro
-class Carro extends Automovel {
-    constructor(cor, modelo, combustivel) {
-        super(cor, modelo, combustivel, 4);
-        this.ligado = false;
-        this.vidroAberto = false;
-    }
+// // Classe filha: Carro
+// class Carro extends Automovel {
+//     constructor(cor, modelo, combustivel) {
+//         super(cor, modelo, combustivel, 4);
+//         this.ligado = false;
+//         this.vidroAberto = false;
+//     }
 
-    ligar() {
-        if (!this.ligado) {
-            this.ligado = true;
-            console.log("Carro ligado.");
+//     ligar() {
+//         if (!this.ligado) {
+//             this.ligado = true;
+//             console.log("Carro ligado.");
+//         } else {
+//             console.log("Carro já está ligado.");
+//         }
+//     }
+
+//     desligar() {
+//         if (this.ligado) {
+//             this.ligado = false;
+//             console.log("Carro desligado.");
+//         } else {
+//             console.log("Carro já está desligado.");
+//         }
+//     }
+
+//     abrirVidro() {
+//         if (!this.vidroAberto) {
+//             this.vidroAberto = true;
+//             console.log("Vidro aberto.");
+//         } else {
+//             console.log("Vidro já está aberto.");
+//         }
+//     }
+
+//     fecharVidro() {
+//         if (this.vidroAberto) {
+//             this.vidroAberto = false;
+//             console.log("Vidro fechado.");
+//         } else {
+//             console.log("Vidro já está fechado.");
+//         }
+//     }
+// }
+
+// class Moto extends Automovel {
+//     constructor(cor, modelo, combustivel) {
+//         super(cor, modelo, combustivel, 2);
+//     }
+// }
+
+// class Caminhao extends Automovel {
+//     constructor(cor, modelo, combustivel) {
+//         super(cor, modelo, combustivel, 6);
+//     }
+// }
+
+// const meuCarro = new Carro("Vermelho", "Sedan X", "Gasolina");
+// console.log(meuCarro.info());
+
+// meuCarro.ligar();
+// meuCarro.abrirVidro();
+// meuCarro.fecharVidro();
+// meuCarro.desligar();
+
+// const minhaMoto = new Moto("Preta", "Moto Y", "Etanol");
+// console.log(minhaMoto.info());
+
+// const meuCaminhao = new Caminhao("Branco", "Truck Z", "Diesel");
+// console.log(meuCaminhao.info());
+
+
+
+//Exercicio 4
+const { resolve } = require('path');
+const readline = require('readline');
+ // cria interface de leitura
+ const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+ });
+
+ class Cliente {
+    constructor(nome, profissao, saldo) {
+        this.nome = nome;
+        this.profissao = profissao;
+        this.saldo = saldo;
+    }
+    pix(valor){
+        if(valor > 0 && valor <= this.saldo){
+            this.saldo -= valor;
+            console.log("Piquis enviado com sucesso");
         } else {
-            console.log("Carro já está ligado.");
+            console.log("Saldo insuficiente haha");
         }
     }
 
-    desligar() {
-        if (this.ligado) {
-            this.ligado = false;
-            console.log("Carro desligado.");
+    emprestimo(valor){
+        if(valor > 0){
+            this.saldo += valor;
+            console.log("Empréstimo realizado com sucesso");
         } else {
-            console.log("Carro já está desligado.");
+            console.log("Valor de empréstimo inválido");
         }
     }
 
-    abrirVidro() {
-        if (!this.vidroAberto) {
-            this.vidroAberto = true;
-            console.log("Vidro aberto.");
+    saque(valor){
+        if(valor > 0 && valor <= this.saldo){
+            this.saldo -= valor;
+            console.log("Saque realizado com sucesso")
         } else {
-            console.log("Vidro já está aberto.");
+            console.log("Valor indisponivel")
         }
     }
 
-    fecharVidro() {
-        if (this.vidroAberto) {
-            this.vidroAberto = false;
-            console.log("Vidro fechado.");
-        } else {
-            console.log("Vidro já está fechado.");
-        }
+    extrato(valor){
+        console.log("===EXTRATO===")
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Profissão: ${this.profissao}`)
+        console.log(`Saldo: ${this.saldo}`)
     }
-}
+ }
 
-class Moto extends Automovel {
-    constructor(cor, modelo, combustivel) {
-        super(cor, modelo, combustivel, 2);
-    }
-}
+ function perguntar(pergunta){
+    return new Promise(resolve => rl.question(pergunta, resposta => resolve(resposta)));
+ }
 
-class Caminhao extends Automovel {
-    constructor(cor, modelo, combustivel) {
-        super(cor, modelo, combustivel, 6);
-    }
-}
-
-const meuCarro = new Carro("Vermelho", "Sedan X", "Gasolina");
-console.log(meuCarro.info());
-
-meuCarro.ligar();
-meuCarro.abrirVidro();
-meuCarro.fecharVidro();
-meuCarro.desligar();
-
-const minhaMoto = new Moto("Preta", "Moto Y", "Etanol");
-console.log(minhaMoto.info());
-
-const meuCaminhao = new Caminhao("Branco", "Truck Z", "Diesel");
-console.log(meuCaminhao.info());
+ async function inicarBanco() {
+    const nome = await perguntar("Digite seu nome: ")
+ }
