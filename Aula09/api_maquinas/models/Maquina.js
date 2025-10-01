053f4ca2a4bb264c5fd9cc168af08ce2545c83d7
tmarques2 <thainaramarques258@gmail.com>
2025-10-01 19:54:10 -0300

    aula 9

---
+++ Aula09/api_maquinas/models/Maquina.js
@@ -0,0 +1,14 @@
+// importando biblioteca do mongoose
+const mongoose = require("mongoose");
+
+// cria a estrutura (schema) para o documento da maquina
+const MaquinaSchema = new mongoose.Schema({
+    nome:{type: String, required:true},
+    tipo:{type:String, required: true},
+    status:{type:String, required: true},
+    ultimaManutencao:{type:Date, required: true},
+    proximaManutencao:{type:Date, required: true},
+})
+
+// exporta o modelo
+module.exports = mongoose.model("Maquina",MaquinaSchema);
